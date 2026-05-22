f60d2c5e680b0904d5c3e4e60848426da15fe199
Francis <Z.Zheng-13@student.tudelft.nl>
2026-05-22 16:41:22 +0200

    Add filtering before save, and show all commands in readme.md, Adjust mapping_management_node

--- mdp_mapping/mdp_mapping/mapping_manager_node.py
+++ mdp_mapping/mdp_mapping/mapping_manager_node.py
@@ -10,23 +10,10 @@ from rclpy.node import Node
 from sensor_msgs.msg import LaserScan
 from nav_msgs.msg import Odometry, OccupancyGrid
 from std_srvs.srv import Trigger
+from mdp_mapping.map_filter import filter_map_pgm, write_filtered_yaml
 
 
 class MappingManagerNode(Node):
-    """
-    Mapping manager for the greenhouse robot.
-
-    This node does not implement SLAM itself.
-    It monitors the required mapping topics and provides a service to save the map.
-
-    Required inputs:
-        /scan
-        /odom
-        /map
-
-    SLAM is provided by slam_toolbox.
-    Map saving is handled by nav2_map_server map_saver_cli.
-    """
 
     def __init__(self):
         super().__init__("mapping_manager_node")
@@ -37,8 +24,15 @@ class MappingManagerNode(Node):
         self.declare_parameter("odom_topic", "/odom")
         self.declare_parameter("map_topic", "/map")
 
+        self.declare_parameter("enable_filtering", True)
+        self.declare_parameter("min_obstacle_area", 8)
+        self.declare_parameter("filter_kernel_size", 3)
+
         self.map_save_dir = self.get_parameter("map_save_dir").value
         self.map_name = self.get_parameter("map_name").value
+        self.enable_filtering = self.get_parameter("enable_filtering").value
+        self.min_obstacle_area = self.get_parameter("min_obstacle_area").value
+        self.filter_kernel_size = self.get_parameter("filter_kernel_size").value
 
         self.scan_received = False
         self.odom_received = False
@@ -47,6 +41,7 @@ class MappingManagerNode(Node):
         scan_topic = self.get_parameter("scan_topic").value
         odom_topic = self.get_parameter("odom_topic").value
         map_topic = self.get_parameter("map_topic").value
+        
 
         self.create_subscription(
             LaserScan,
@@ -116,15 +111,29 @@ class MappingManagerNode(Node):
             return response
 
         if self.map_save_dir == "":
-            package_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-            self.map_save_dir = os.path.join(package_dir, "maps")
+            # Default runtime map directory:
+            # ~/ros2_ws/maps/mdp_mapping
+            home_dir = os.path.expanduser("~")
+            self.map_save_dir = os.path.join(
+                home_dir,
+                "ros2_ws",
+                "maps"
+            )
 
-        os.makedirs(self.map_save_dir, exist_ok=True)
+        posegraph_dir = os.path.join(self.map_save_dir, "posegraph")
+        raw_map_dir = os.path.join(self.map_save_dir, "raw")
+        filtered_map_dir = os.path.join(self.map_save_dir, "filtered")
+
+        os.makedirs(posegraph_dir, exist_ok=True)
+        os.makedirs(raw_map_dir, exist_ok=True)
+        os.makedirs(filtered_map_dir, exist_ok=True)
 
         timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
-        map_base_path = os.path.join(
-            self.map_save_dir,
-            f"{self.map_name}_{timestamp}"
+        map_base_name = f"{self.map_name}_{timestamp}"
+
+        raw_map_base_path = os.path.join(
+            raw_map_dir,
+            f"{map_base_name}_raw"
         )
 
         command = [
@@ -133,15 +142,55 @@ class MappingManagerNode(Node):
             "nav2_map_server",
             "map_saver_cli",
             "-f",
-            map_base_path,
+            raw_map_base_path,
             "--ros-args",
             "-p",
             "map_subscribe_transient_local:=true",
         ]
 
-        self.get_logger().info(f"Saving map to: {map_base_path}.yaml / .pgm")
+        self.get_logger().info(
+            f"Saving raw map to: {raw_map_base_path}.yaml / .pgm"
+        )
 
         try:
+            # save posegraph and map sequentially to avoid slam_toolbox serialize_map service conflicts with map_saver_cli
+            posegraph_base_path = os.path.join(
+                posegraph_dir,
+                map_base_name
+            )
+
+            serialize_command = [
+                "ros2",
+                "service",
+                "call",
+                "/slam_toolbox/serialize_map",
+                "slam_toolbox/srv/SerializePoseGraph",
+                f"{{filename: '{posegraph_base_path}'}}",
+            ]
+
+            self.get_logger().info(
+                f"Saving slam_toolbox posegraph to: {posegraph_base_path}"
+            )
+
+            serialize_result = subprocess.run(
+                serialize_command,
+                stdout=subprocess.PIPE,
+                stderr=subprocess.PIPE,
+                text=True,
+                timeout=30,
+            )
+
+            if serialize_result.returncode != 0:
+                response.success = False
+                response.message = f"Posegraph saving failed: {serialize_result.stderr}"
+                self.get_logger().error(response.message)
+                return response
+
+            self.get_logger().info(
+                f"Posegraph saved successfully: {posegraph_base_path}"
+            )
+
+
             result = subprocess.run(
                 command,
                 stdout=subprocess.PIPE,
@@ -150,14 +199,56 @@ class MappingManagerNode(Node):
                 timeout=20,
             )
 
-            if result.returncode == 0:
-                response.success = True
-                response.message = f"Map saved successfully: {map_base_path}.yaml"
-                self.get_logger().info(response.message)
-            else:
+            if result.returncode != 0:
                 response.success = False
                 response.message = f"Map saving failed: {result.stderr}"
                 self.get_logger().error(response.message)
+                return response
+
+            raw_pgm_path = raw_map_base_path + ".pgm"
+            raw_yaml_path = raw_map_base_path + ".yaml"
+
+            self.get_logger().info(f"Raw map saved successfully: {raw_yaml_path}")
+
+            if not self.enable_filtering:
+                response.success = True
+                response.message = (
+                    "Posegraph and raw map saved successfully. Filtering disabled.\n"
+                    f"Posegraph base path: {posegraph_base_path}\n"
+                    f"Raw map: {raw_yaml_path}"
+                )
+                return response
+
+            filtered_map_base_path = os.path.join(
+                filtered_map_dir,
+                f"{map_base_name}_filtered"
+            )
+
+            filtered_pgm_path = filtered_map_base_path + ".pgm"
+            filtered_yaml_path = filtered_map_base_path + ".yaml"
+
+            filter_map_pgm(
+                input_pgm=raw_pgm_path,
+                output_pgm=filtered_pgm_path,
+                min_obstacle_area=int(self.min_obstacle_area),
+                kernel_size=int(self.filter_kernel_size),
+            )
+
+            write_filtered_yaml(
+                raw_yaml_path=raw_yaml_path,
+                filtered_yaml_path=filtered_yaml_path,
+                filtered_pgm_path=filtered_pgm_path,
+            )
+
+            response.success = True
+            response.message = (
+                "Posegraph, raw map, and filtered map saved successfully.\n"
+                f"Posegraph base path: {posegraph_base_path}\n"
+                f"Raw map: {raw_yaml_path}\n"
+                f"Filtered map: {filtered_yaml_path}"
+            )
+
+            self.get_logger().info(response.message)
 
         except subprocess.TimeoutExpired:
             response.success = False
@@ -166,11 +257,10 @@ class MappingManagerNode(Node):
 
         except Exception as e:
             response.success = False
-            response.message = f"Map saving error: {str(e)}"
+            response.message = f"Map saving/filtering error: {str(e)}"
             self.get_logger().error(response.message)
 
         return response
-
 
 def main(args=None):
     rclpy.init(args=args)
