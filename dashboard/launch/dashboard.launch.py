e4fb05cead98cd10dbd95848504ae6b68bc5ee56
Moniek Tummers <m.h.tummers@student.tudelft.nl>
2026-05-28 12:06:40 +0200

    put the dashboard into a package with a launch file for both the dashboard and rosbridge

---
+++ dashboard/launch/dashboard.launch.py
@@ -0,0 +1,35 @@
+import os
+from launch import LaunchDescription
+from launch.actions import ExecuteProcess
+from launch_ros.actions import Node
+
+
+def generate_launch_description():
+
+    # Path to your React dashboard
+    dashboard_path = os.path.join(
+        os.getenv("HOME"),
+        "ros2_ws/src/mdp_mirte_master/dashboard/mirte_dashboard"
+    )
+
+    # Start React (Vite dev server)
+    dashboard = ExecuteProcess(
+        cmd=[
+            "bash",
+            "-c",
+            f"cd {dashboard_path} && npm run dev -- --host"
+        ],
+        output="screen"
+    )
+
+    # rosbridge
+    rosbridge = Node(
+        package="rosbridge_server",
+        executable="rosbridge_websocket",
+        output="screen"
+    )
+
+    return LaunchDescription([
+        rosbridge,
+        dashboard
+    ])
