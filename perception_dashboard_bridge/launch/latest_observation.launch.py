983b3c090a976d96b2c0feb8dfff8c0749704358
Bram Kalkman <bakalkman@tudelft.nl>
2026-06-09 11:24:51 +0200

    fixed absolute path generation

--- perception_dashboard_bridge/launch/latest_observation.launch.py
+++ perception_dashboard_bridge/launch/latest_observation.launch.py
@@ -1,10 +1,18 @@
 #!/usr/bin/env python3
 
+import os
+
 from launch import LaunchDescription
 from launch_ros.actions import Node
 
+snapshot_dir = os.path.join(
+    os.path.expanduser('~/ros2_ws'),
+    'perception_snapshots'
+)
+
 
 def generate_launch_description():
+    print("snapshot_dir:", snapshot_dir, type(snapshot_dir))
     return LaunchDescription([
         Node(
             package='perception_dashboard_bridge',
@@ -12,7 +20,7 @@ def generate_launch_description():
             name='latest_observation_publisher',
             output='screen',
             parameters=[{
-                'snapshot_dir': '/home/nikolaos/ros2_ws/perception_snapshots',
+                'snapshot_dir': snapshot_dir,
                 'publish_topic': '/latest_observation',
                 'publish_rate_hz': 1.0,
                 'base_url': 'http://localhost:8088',
